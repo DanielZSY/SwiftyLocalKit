fc0f07604efc3209c05a0003f89e966f098f583c
YuLe <>
2021-05-14 14:15:16 +0800

    1.0.0

---
+++ SwiftyLocalKit/Example/Pods/Target Support Files/Pods-SwiftyLocalKit_Example/Pods-SwiftyLocalKit_Example-umbrella.h
@@ -0,0 +1,16 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+
+FOUNDATION_EXPORT double Pods_SwiftyLocalKit_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_SwiftyLocalKit_ExampleVersionString[];
+
